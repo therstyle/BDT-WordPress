fd27f79c0986981ce13ef4cf07fc703ec4c1fc95
Wojciech Laszczyk <support@chop-chop.org>
2019-06-19 12:24:20 +0200

    Add logs to debug newsletter

---
+++ wp-content/themes/bostonducktours/blocks/js/modern/44.bundle.98bb0cf902b238caaa51.js
@@ -0,0 +1,12 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{131:function(n,r,e){var t;
+/*!
+  Copyright (c) 2017 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+/*!
+  Copyright (c) 2017 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+!function(){"use strict";var e={}.hasOwnProperty;function i(){for(var n=[],r=0;r<arguments.length;r++){var t=arguments[r];if(t){var a=typeof t;if("string"===a||"number"===a)n.push(t);else if(Array.isArray(t)&&t.length){var o=i.apply(null,t);o&&n.push(o)}else if("object"===a)for(var s in t)e.call(t,s)&&t[s]&&n.push(s)}}return n.join(" ")}n.exports?(i.default=i,n.exports=i):void 0===(t=function(){return i}.apply(r,[]))||(n.exports=t)}()},373:function(n,r,e){var t=e(374);"string"==typeof t&&(t=[[n.i,t,""]]);var i={hmr:!0,transform:void 0,insertInto:void 0};e(88)(t,i);t.locals&&(n.exports=t.locals)},374:function(n,r,e){(n.exports=e(87)(!1)).push([n.i,".Image img {\n  display: block;\n  height: auto;\n  max-width: 100%; }\n",""])},721:function(n,r,e){"use strict";e.r(r);var t=e(1),i=e.n(t),a=e(0),o=e.n(a),s=e(131),l=e.n(s),p=(e(373),{align:o.a.oneOf(["","left","center","right"]),innerHTML:o.a.string}),u=function(n){var r=n.align,e=n.innerHTML;return e.trim()?i.a.createElement("div",{className:l()("Image",r),dangerouslySetInnerHTML:{__html:e}}):null};u.propTypes=p,u.defaultProps={align:"",innerHTML:""},r.default=u}}]);
