62df3857dea70fb42a3fac5106ff454926af11a4
Rufat <rufat@829llc.com>
2019-09-05 15:08:56 -0400

    GTM

--- wp-content/themes/bostonducktours/index.php
+++ wp-content/themes/bostonducktours/index.php
@@ -30,12 +30,15 @@ $js->load();
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Google Tag Manager --> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-T2P2BS2');</script> <!-- End Google Tag Manager --> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<?php wp_head(); ?>
 </head>
 
 <body class="<?php echo \BostonDuckTours\Browsers::is_IE() ? 'internet-explorer' : ''; ?>">
+	<!-- Google Tag Manager (noscript) --> <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2P2BS2" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> <!-- End Google Tag Manager (noscript) --> 
+
 <div id="app-root"></div>
 
 <?php wp_footer(); ?>
